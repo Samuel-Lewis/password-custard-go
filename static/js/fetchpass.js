3c720da8f0ec390b8ec684541ee4a91ea4587eea
Samuel-Lewis <mazuel@gmail.com>
2017-09-06 14:47:58 +0930

    Added history of previous passwords

--- static/js/fetchpass.js
+++ static/js/fetchpass.js
@@ -1,14 +1,30 @@
+const history = [];
+
 $(document).ready(function() {
+	
 	$("#generate-password").on('click', function() {
 		$.ajax({
 			url: "/raw",
 			method: "GET",
 			success: function(data) {
-				
-				$("#password-field").html($("<div />").text(data).html());
+				pass = $("<div />").text(data).html();
+				// Send to heading
+				$("#password-field").html(pass);
+
+				logPass(pass);
 			},
 		});
 	});
 
 	$("#generate-password").click()
 });
+
+function logPass(pass) {
+	$("#history").html(history.join('</br>'));
+
+	history.unshift(pass);
+
+	if (history.length > 10) {
+		history.pop();
+	}
+}
