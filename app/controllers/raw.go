2f8b696a9bc25a5667d62414bab7a94de070f0ae
Sam Lewis <mazuel@gmail.com>
2018-07-06 12:30:25 +0930

    Switch to polling method for features
    Allows topo sorting of features

--- app/controllers/raw.go
+++ app/controllers/raw.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"fmt"
-	"math/rand"
 	"net/http"
 	"strings"
 
@@ -23,61 +21,23 @@ func Raw(w http.ResponseWriter, r *http.Request) {
 // GeneratePassword makes the password
 func generatePassword() string {
 
-	phrase := []string{""}
+	// Polled features
+	var feats []string
+	feats = append(feats, feature.Choose("word"))
+	feats = append(feats, feature.Choose("word"))
+	feats = append(feats, feature.Choose("word"))
+	feats = append(feats, feature.Choose("symbol"))
+	feats = append(feats, feature.Choose("uppercase"))
+	feats = append(feats, feature.Choose("titlecase"))
 
-	phrase = feature.Get("noun")(phrase)
-	phrase = feature.Get("verb")(phrase)
-	phrase = feature.Get("adjective")(phrase)
+	// Orders application of features
+	feats = feature.Order(feats)
 
-	phrase = feature.Get("symbolsimple")(phrase)
+	// Apply features
+	phrase := []string{}
+	for _, f := range feats {
+		phrase = feature.Apply(f)(phrase)
+	}
 
 	return strings.Join(phrase, "")
-}
-
-// capitalize random word and title casing
-func capitalize(phrase []string) []string {
-	// Title case everything
-	var newPhrase []string
-	for _, x := range phrase {
-		newPhrase = append(newPhrase, strings.Title(x))
-	}
-
-	phrase = newPhrase
-	randWord := rand.Intn(len(phrase) + 1)
-
-	// No block capitals, title case only
-	if randWord == len(phrase) {
-		return phrase
-	}
-
-	// Upper case a word (remove title on following)
-	phrase[randWord] = strings.ToUpper(phrase[randWord])
-	randWord = (randWord + 1) % len(phrase)
-	phrase[randWord] = strings.ToLower(phrase[randWord])
-
-	return phrase
-}
-
-// leet chars on random values
-func leet(phrase []string) []string {
-	// repeats := rand.Intn(len(phrase) - 1)
-	// for i := 1; i <= repeats; i++ {
-	// 	randWord := rand.Intn(len(phrase))
-
-	// }
-
-	return phrase
-}
-
-// Number adds random number between words
-func insert(phrase []string, in string) []string {
-	word := rand.Intn(len(phrase) + 1)
-
-	if word == len(phrase) {
-		phrase[word-1] = fmt.Sprint(phrase[word-1], in)
-	} else {
-		phrase[word] = fmt.Sprint(in, phrase[word])
-	}
-
-	return phrase
 }
--- app/models/feature/feature.go
+++ app/models/feature/feature.go
@@ -3,12 +3,15 @@ package feature
 import (
 	"log"
 	"math/rand"
+	"sort"
 )
 
 // Applier is function type for function to take the current password, manipulate, and return it
 type Applier func([]string) []string
 
 var features map[string]Applier
+var groups map[string][]string
+var order []string
 
 // Nop is the default and performs no operation to the password
 func Nop(s []string) []string {
@@ -18,18 +21,44 @@ func Nop(s []string) []string {
 
 // Register builds the feature map from string to feature implementations
 func Register() {
+	groups = make(map[string][]string)
 	features = make(map[string]Applier)
+
+	groups["word"] = []string{"noun", "verb", "adjective"}
+	groups["symbol"] = []string{"symbolsimple", "symbolrandom", "symbolreplace"}
+	groups["capitalise"] = []string{"titlecase", "uppercase"}
 
 	features["noun"] = Noun
 	features["verb"] = Verb
 	features["adjective"] = Adjective
 	features["symbolsimple"] = SymbolSimple
+	features["symbolrandom"] = SymbolRandom
+	features["symbolreplace"] = SymbolReplace
+	features["titlecase"] = TitleCase
+	features["uppercase"] = UpperCase
+
+	order = []string{
+		"noun", "verb", "adjective",
+		"titlecase", "uppercase",
+		"leet",
+		"numberreplace", "symbolreplace",
+		"numberrandom", "symbolrandom",
+		"numbersimple", "symbolsimple",
+	}
 
 	log.Printf("Registered %d features", len(features))
 }
 
-// Get finds requested feature and returns t
-func Get(s string) Applier {
+// Choose selects a random from a group, or if not a group, returns it self
+func Choose(s string) string {
+	if val, ok := groups[s]; ok {
+		return val[rand.Intn(len(val))]
+	}
+	return s
+}
+
+// Apply finds requested feature and returns t
+func Apply(s string) Applier {
 	if val, ok := features[s]; ok {
 		return val
 	}
@@ -39,13 +68,47 @@ func Get(s string) Applier {
 // Insert value at random position in password
 func Insert(s string, p []string) []string {
 	if len(p) == 0 {
-		log.Printf("len(p) == 0")
 		return []string{s}
 	}
 
-	i := rand.Intn(len(p))
+	i := rand.Intn(len(p) + 1)
 	p = append(p, "")
 	copy(p[i+1:], p[i:])
 	p[i] = s
 	return p
+}
+
+type byOrder []string
+
+func (s byOrder) Len() int {
+	return len(s)
+}
+
+func (s byOrder) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s byOrder) Less(i, j int) bool {
+	// true if s[i] should come before s[j]
+	if s[i] == s[j] {
+		return true
+	}
+
+	// Check compared to list
+	for _, val := range order {
+		if val == s[i] {
+			return true
+		}
+
+		if val == s[j] {
+			return false
+		}
+	}
+	return true
+}
+
+// Order sorts the methods to apply according to topo order
+func Order(s []string) []string {
+	sort.Sort(byOrder(s))
+	return s
 }
